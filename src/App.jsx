76b2da4b1bf4f72c0acd0737a4c0144c4da706b5
Laksh <yadavlakshya86@gmail.com>
2023-12-20 15:13:55 +0530

    docs-app-creaation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full relative h-screen bg-zinc-800'>
+      <div className='w-full absolute top-[5%] py-10  flex justify-center text-zinc-600 text-xl font-semibold'>Document</div>
+      <h1 className='text-[13vw] absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold leading-none tracking-tighter'>Docs</h1>
+    </div>
+  )
+}
+
+export default App
