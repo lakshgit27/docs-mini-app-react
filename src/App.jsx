36afb52d99bafc2bd70f6e53c173ca56e17a2c44
Laksh <yadavlakshya86@gmail.com>
2023-12-21 08:21:21 +0530

    final code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Background from './components/Background'
 
 const App = () => {
   return (
     <div className='w-full relative h-screen bg-zinc-800'>
-      <div className='w-full absolute top-[5%] py-10  flex justify-center text-zinc-600 text-xl font-semibold'>Document</div>
-      <h1 className='text-[13vw] absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold leading-none tracking-tighter'>Docs</h1>
+      <Background/>
+      <div className='  fixed top-0 z-[3]  w-full h-screen bg-sky-800/50'></div>
+     
     </div>
   )
 }
---
+++ src/components/Background.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Background = () => {
+  return (
+    <>
+    <div className='fixed z-[2] w-full h-screen' >
+    <div className='w-full absolute top-[5%] py-10  flex justify-center text-zinc-600 text-xl font-semibold'>Document</div>
+      <h1 className='text-[13vw] absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold leading-none tracking-tighter'>Docs</h1>
+
+    </div>
+    
+
+    
+    </>
+  )
+}
+
+export default Background
